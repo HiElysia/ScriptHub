74e74fbc2ee6829a955ca78249162572371bd87f
HiElysia <149615479+HiElysia@users.noreply.github.com>
2024-11-13 23:56:29 +0800

    Update webchrome.sh

--- base/webchrome.sh
+++ base/webchrome.sh
@@ -5,7 +5,8 @@ sudo apt install docker.io
 sudo docker pull kasmweb/chrome:1.16.0
 
 if [ -n "$2" ]; then
-    echo "Depoly Docker VNC With Password"
+    echo "Depoly Docker VNC With Password and Chrome Dir"
+    mkdir -p $2
     sudo docker run --rm -d --shm-size=1024m -p 6901:6901 -v $2:/var/tmp -e VNC_PW=$1 -e APP_ARGS=--user-data-dir=/var/tmp kasmweb/chrome:1.16.0
 elif [ -n "$1" ]; then
     echo "Depoly Docker VNC With Password"
