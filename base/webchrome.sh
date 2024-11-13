0595a312f76f7e80d3669b47ffc0c21debb60306
HiElysia <149615479+HiElysia@users.noreply.github.com>
2024-11-13 23:53:24 +0800

    Update webchrome.sh

--- base/webchrome.sh
+++ base/webchrome.sh
@@ -6,11 +6,11 @@ sudo docker pull kasmweb/chrome:1.16.0
 
 if [ -n "$2" ]; then
     echo "Depoly Docker VNC With Password"
-    sudo docker run --rm -d --shm-size=512m -p 6901:6901 -v $2:/var/tmp -e VNC_PW=$1 -e APP_ARGS=--user-data-dir=/var/tmp kasmweb/chrome:1.16.0
+    sudo docker run --rm -d --shm-size=1024m -p 6901:6901 -v $2:/var/tmp -e VNC_PW=$1 -e APP_ARGS=--user-data-dir=/var/tmp kasmweb/chrome:1.16.0
 elif [ -n "$1" ]; then
     echo "Depoly Docker VNC With Password"
-    sudo docker run --rm -d --shm-size=512m -p 6901:6901 -e VNC_PW=$1 kasmweb/chrome:1.16.0
+    sudo docker run --rm -d --shm-size=1024m -p 6901:6901 -e VNC_PW=$1 kasmweb/chrome:1.16.0
 else
     echo "Depoly Docker for No-VNC"
-    sudo docker run --rm -d --shm-size=512m -p 6901:6901 kasmweb/chrome:1.16.0
+    sudo docker run --rm -d --shm-size=1024m -p 6901:6901 kasmweb/chrome:1.16.0
 fi
