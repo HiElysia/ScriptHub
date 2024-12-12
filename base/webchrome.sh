1a6ee7609cb8f620aac574ac34ce3b7ab01cafaa
HiElysia <149615479+HiElysia@users.noreply.github.com>
2024-12-12 17:06:41 +0800

    Update webchrome.sh

--- base/webchrome.sh
+++ base/webchrome.sh
@@ -2,7 +2,7 @@
 
 sudo apt update
 sudo apt install docker.io
-sudo docker pull kasmweb/chrome:1.16.0
+sudo docker pull kasmweb/chrome
 
 if [ -n "$2" ]; then
     echo "Depoly Docker VNC With Password and Chrome Dir"
@@ -11,11 +11,17 @@ if [ -n "$2" ]; then
     current_dir=$(dirname "$(realpath "$0")")
     data_dir="$current_dir/$2"
     echo $data_dir
-    sudo docker run --rm -d --shm-size=1024m -p 6901:6901 -v $data_dir:/home/kasm-user -e VNC_PW=$1 kasmweb/chrome:1.16.0
+
+    
+    if [ -n "$3" ]; then
+        sudo docker run --rm -d --shm-size=1024m -p 6901:6901 -v $data_dir:/home/kasm-user -e VNC_PW=$1 APP_ARGS=--remote-debugging-port=9222 kasmweb/chrome
+    else
+        sudo docker run --rm -d --shm-size=1024m -p 6901:6901 -v $data_dir:/home/kasm-user -e VNC_PW=$1 kasmweb/chrome
+    fi
 elif [ -n "$1" ]; then
     echo "Depoly Docker VNC With Password"
-    sudo docker run --rm -d --shm-size=1024m -p 6901:6901 -e VNC_PW=$1 kasmweb/chrome:1.16.0
+    sudo docker run --rm -d --shm-size=1024m -p 6901:6901 -e VNC_PW=$1 kasmweb/chrome
 else
     echo "Depoly Docker for No-VNC"
-    sudo docker run --rm -d --shm-size=1024m -p 6901:6901 kasmweb/chrome:1.16.0
+    sudo docker run --rm -d --shm-size=1024m -p 6901:6901 kasmweb/chrome
 fi
